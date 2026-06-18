482aae662872c8d81a21cf500ec793b710812e34
Richard Olivey <richard@olivey.net>
2026-06-18 20:49:37 +0100

    Adding live settings

---
+++ web/sites/default/settings.live.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Environment Indicator
+ */
+$config['environment_indicator.indicator']['fg_color'] = '#ffffff';
+$config['environment_indicator.indicator']['bg_color'] = '#aa0000';
+$config['environment_indicator.indicator']['name'] = 'Live';
+
+/**
+ * Private files Directory
+ */
+$settings['file_private_path'] = ''; // add private directory here. Jelastic is usually: /var/www/webroot/ROOT/private or /var/www/webroot/private
+
+/**
+ * Config sync directory
+ */
+$settings['config_sync_directory'] = '../config/sync';
